8615936bae056c41e6fe3c881b31bd9f068cc7ea
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-05-04 17:55:29 +0100

    img fix attempt

--- src/footer.tsx
+++ src/footer.tsx
@@ -2,16 +2,16 @@ export default function Footer() {
   return (
     <div className="items flex justify-center gap-4 rounded-b-2xl bg-[#161619] bg-center p-4 ">
       <a href="#blank">
-        <img src="/Facebook Icon.png" alt="" />
+        <img src="./Facebook Icon.png" alt="" />
       </a>
       <a href="#blank">
-        <img src="/Instagram Icon.png" alt="" />
+        <img src="./Instagram Icon.png" alt="" />
       </a>
       <a href="#blank">
-        <img src="/Twitter Icon.png" alt="" />
+        <img src="./Twitter Icon.png" alt="" />
       </a>
       <a href="#blank">
-        <img src="/GitHub Icon.png" alt="" />
+        <img src="./GitHub Icon.png" alt="" />
       </a>
     </div>
   );
--- src/img.tsx
+++ src/img.tsx
@@ -1,7 +1,7 @@
 export default function Img() {
   return (
     <img
-      src="/bailey-burton-uuK-Q_3ER-I-unsplash.jpg"
+      src="./bailey-burton-uuK-Q_3ER-I-unsplash.jpg"
       className="h-[317px] w-[317px] rounded-t-2xl"
     ></img>
   );
