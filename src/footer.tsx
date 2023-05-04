b3d0a2de8f000e6f61a03110d3ceed93109a0cc6
MSzoot <97002088+MSzoot@users.noreply.github.com>
2023-05-04 17:47:55 +0100

    img fix

--- src/footer.tsx
+++ src/footer.tsx
@@ -2,19 +2,17 @@ export default function Footer() {
   return (
     <div className="items flex justify-center gap-4 rounded-b-2xl bg-[#161619] bg-center p-4 ">
       <a href="#blank">
-        <img src="/images/Facebook Icon.png" alt="" />
+        <img src="/Facebook Icon.png" alt="" />
       </a>
       <a href="#blank">
-        <img src="/images/Instagram Icon.png" alt="" />
+        <img src="/Instagram Icon.png" alt="" />
       </a>
       <a href="#blank">
-        <img src="/images/Twitter Icon.png" alt="" />
+        <img src="/Twitter Icon.png" alt="" />
       </a>
       <a href="#blank">
-        <img src="/images/GitHub Icon.png" alt="" />
+        <img src="/GitHub Icon.png" alt="" />
       </a>
     </div>
   );
 }
-
-
